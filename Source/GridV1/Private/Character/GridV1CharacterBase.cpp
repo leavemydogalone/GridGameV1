7e15ed3695bc0b5791507bdff6dcd4bf63d8e171
leavemydogalone <niklasport88@gmail.com>
2025-09-10 09:22:33 -0700

    new character

--- Source/GridV1/Private/Character/GridV1CharacterBase.cpp
+++ Source/GridV1/Private/Character/GridV1CharacterBase.cpp
@@ -6,7 +6,6 @@
 // Sets default values
 AGridV1CharacterBase::AGridV1CharacterBase()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 }
@@ -22,13 +21,6 @@ void AGridV1CharacterBase::BeginPlay()
 void AGridV1CharacterBase::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
-}
-
-// Called to bind functionality to input
-void AGridV1CharacterBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
 }
 
